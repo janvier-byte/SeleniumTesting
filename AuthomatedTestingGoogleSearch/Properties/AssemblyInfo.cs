9a8324f4ffb474ee253f407023ea8830550dc070
KARANGWA Janvier <janvier@techaffinity.com>
2022-12-02 09:08:54 +0200

    first commit

---
+++ AuthomatedTestingGoogleSearch/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AuthomatedTestingGoogleSearch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AuthomatedTestingGoogleSearch")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("edac6940-5a79-42d6-8d5a-7fe3d69167bd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AuthomatedTestingGoogleSearch/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace AuthomatedTestingGoogleSearch
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int waitingTime = 1000;
+            By googleSearchBar = By.Name("q");
+            By googleSearchButton = By.Name("btnK");
+            By googleResultText = By.XPath(".//h2//span[text()='Street Fighter V']");
+            By link = By.XPath(".//h3[text()='Street Fighter Series | CAPCOM']");
+            By game = By.XPath(".//span[text()='GAMES']");
+            By submenu = By.XPath(".//a[text()='Street Fighter V: Champion Edition']");
+            By showall = By.XPath(".//a[text()='Allow all']");
+            IWebDriver webDriver = new ChromeDriver();
+            webDriver.Manage().Window.Maximize();
+            Thread.Sleep(waitingTime);
+            webDriver.Navigate().GoToUrl("https://www.google.co.uk");
+            Thread.Sleep(waitingTime);
+            webDriver.FindElement(googleSearchBar).SendKeys("Street Fighter V");
+            Thread.Sleep(waitingTime);
+            webDriver.FindElement(googleSearchButton).Click();
+            var actualResultText = webDriver.FindElement(googleResultText);
+            Assert.IsTrue(actualResultText.Text.Equals("Street Fighter V"));
+            Thread.Sleep(waitingTime);
+            webDriver.FindElement(link).Click();
+            Thread.Sleep(waitingTime);
+            webDriver.FindElement(game).Click();
+            Thread.Sleep(waitingTime);
+            webDriver.FindElement(submenu).Click();
+            Thread.Sleep(waitingTime);
+            webDriver.FindElement(showall).Click();
+            //webDriver.Quit();
+        }
+    }
+}
